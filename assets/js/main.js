b9f478c5bfe859aa33d8866a7ff40f3017996b5e
aKaSteve49m <swastik.hegde2014@gmail.com>
2023-02-11 19:35:13 +0530

    first

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
+      
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
@@ -31,9 +32,17 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
-    const header = document.getElementById('header')
+    const header = document.getElementById('header');
+    const navclr = document.getElementById('clr');
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 80){
+         header.classList.add('scroll-header'); 
+         navclr.classList.add('nav__clr');
+        }
+    else {
+        header.classList.remove('scroll-header');
+        navclr.classList.remove('nav__clr');
+        }
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -68,24 +77,24 @@ const toggleItem = (item) =>{
 }
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight,
+//               sectionTop = current.offsetTop - 58,
+//               sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 function scrollUp(){
@@ -134,9 +143,9 @@ const sr = ScrollReveal({
     // reset: true
 })
 
-sr.reveal(`.home__data`)
-sr.reveal(`.home__img`, {delay: 500})
-sr.reveal(`.home__social`, {delay: 600})
-sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
-sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
-sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
+// sr.reveal(`.home__data`)
+// sr.reveal(`.home__img`, {delay: 500})
+// sr.reveal(`.home__social`, {delay: 600})
+// sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
+// sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
+// sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
